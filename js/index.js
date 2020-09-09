22ec341adfab734a0447c7de3c312cf8d0452743
Atharva-Kulkarni606 <65156793+Atharva-Kulkarni606@users.noreply.github.com>
2020-09-09 08:38:25 -0700

    Project Completed

---
+++ js/index.js
@@ -0,0 +1,18 @@
+
+//Setting the intervalL
+
+setInterval(()=>{
+    let date= new Date();
+    let h= date.getHours();
+    let m=date.getMinutes();
+    let sec= date.getSeconds();
+    let  hRotation= (30*h) + (m/2);
+    let  mRotation= (6*m);
+    let  sRotation= (6*sec);
+
+    //Use Transform Property to rotate the hands
+    hour.style.transform = `rotate(${hRotation}deg)`;
+    minute.style.transform = `rotate(${mRotation}deg)`;
+    seconds.style.transform = `rotate(${sRotation}deg)`;
+
+},1000);
